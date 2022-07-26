e161aed49dbba7b999e09392c0c0c4cfb8cfe538
limbo <yykaue@qq.com>
2022-07-26 19:47:39 +0800

    feat: 增加版本检测功能

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # limbo-common-node
-node
+
+    需要使用node执行的方法
+
+## 安装方法
+
+    npm i limbo-common-node
+
+## 使用说明
---
+++ index.js
@@ -0,0 +1,7 @@
+const checkVersion = require('./lib/checkVersion')
+const getArgv = require('./lib/getArgv')
+const writeVersion = require('./lib/writeVersion')
+
+exports.checkVersion = checkVersion
+exports.getArgv = getArgv
+exports.writeVersion = writeVersion
